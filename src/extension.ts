94db917146f916d60fde2f58c70428cb4d96eade
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-04-14 12:48:29 +0200

    Add support for RootNamespace element. Refactor code.

--- src/extension.ts
+++ src/extension.ts
@@ -4,73 +4,117 @@ import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
     const namespaceCompletionProvider = vscode.languages.registerCompletionItemProvider({ scheme: 'file', language: 'csharp' }, {
-        provideCompletionItems(document, position, token, context) {
-            if (document.fileName.endsWith('.csx')) {
-                return undefined;
+        async provideCompletionItems(document, position, token, context) {
+            if (isScriptFile(document)) {
+                return;
             }
 
-            const linePrefix = document.lineAt(position).text.substr(0, position.character);
-            if (!/^[ \t]*namespace[ \t]+\w*$/.test(linePrefix)) {
-                return undefined;
+            const linePrefix = getLinePrefix(document, position);
+
+            if (!isNamespaceDeclaration(linePrefix)) {
+                return;
             }
 
             const fileDir = path.dirname(document.fileName);
 
-            let searchDir = fileDir;
-            const root = path.parse(fileDir).root;
-
-            let found = false;
-            let hasParentDir = true;
-
-            let csproj = null;
-
-            while (!found && hasParentDir) {
-                const files = fs.readdirSync(searchDir);
-
-                found = files.some(f => f.endsWith('.csproj'));
-                hasParentDir = searchDir !== root;
-
-                if (found) {
-                    csproj = files.find(f => f.endsWith('.csproj'));
-                } else if (hasParentDir) {
-                    searchDir = path.join(searchDir, '..');
-                }
-            }
-
-            if (!found) {
+            const csprojInfo = findCsprojFile(fileDir);
+            if (!csprojInfo) {
                 return undefined;
             }
 
-            const projectRootRelativePath = path.relative(searchDir, fileDir);
-            const csprojBaseName = path.basename(csproj as string, path.extname(csproj as string));
+            let rootNamespace = await getRootNamespaceFromElement(csprojInfo.fullPath);
 
-            const namespace = path.join(csprojBaseName, projectRootRelativePath)
-                .replace(/[\/\\]/g, '.')
-                .replace(/[^\w.]/g, '_')
-                .replace(/[.]{2,}/g, '.')
-                .replace(/^[.]+/, '')
-                .replace(/[.]+$/, '')
-                .split('.')
-                .map(s => /^[0-9]/.test(s) ? '_' + s : s)
-                .join('.');
-
-            const namespacePart = namespace.split('.')[0];
-
-            if (new RegExp(`^[ \t]*namespace[ \t]+${namespacePart}.`).test(linePrefix)) {
-                return undefined;
+            if (!rootNamespace) {
+                rootNamespace = getRootNamespaceFromFileName(csprojInfo.fileName);
             }
 
-            const moduleCompletion = new vscode.CompletionItem(namespace, vscode.CompletionItemKind.Module);
+            const projectRootRelativePath = path.relative(csprojInfo.dir, fileDir);
 
-            const snippetCompletion = new vscode.CompletionItem('namespace-fill', vscode.CompletionItemKind.Snippet);
-            snippetCompletion.insertText = namespace;
-            snippetCompletion.detail = namespace;
+            const namespace = getNamespace(rootNamespace, projectRootRelativePath);
 
-            return [moduleCompletion, snippetCompletion];
+            return getCompletions(namespace);
         }
     });
 
     context.subscriptions.push(namespaceCompletionProvider);
+}
+
+function isScriptFile(document: vscode.TextDocument) {
+    return document.fileName.endsWith('.csx');
+}
+
+function isNamespaceDeclaration(linePrefix: string) {
+    return /^[ \t]*namespace[ \t]+\w*$/.test(linePrefix);
+}
+
+function getLinePrefix(document: vscode.TextDocument, position: vscode.Position) {
+    return document.lineAt(position).text.substr(0, position.character);
+}
+
+function findCsprojFile(fileDir: string) {
+    let searchDir = fileDir;
+    const root = path.parse(fileDir).root;
+
+    let found = false;
+    let hasParentDir = true;
+
+    let csprojFileName: string | undefined;
+
+    while (!found && hasParentDir) {
+        const files = fs.readdirSync(searchDir);
+
+        found = files.some(f => f.endsWith('.csproj'));
+        hasParentDir = searchDir !== root;
+
+        if (found) {
+            csprojFileName = files.find(f => f.endsWith('.csproj'));
+        } else if (hasParentDir) {
+            searchDir = path.join(searchDir, '..');
+        }
+    }
+
+    if (!found || !csprojFileName) {
+        return;
+    }
+
+    return { fileName: csprojFileName, dir: searchDir, fullPath: path.join(searchDir, csprojFileName) };
+}
+
+async function getRootNamespaceFromElement(csprojPath: string) {
+    const csproj = await vscode.workspace.openTextDocument(csprojPath);
+    const matches = csproj.getText().match(/<RootNamespace>([\w.]+)<\/RootNamespace>/);
+
+    if (!matches) {
+        return;
+    }
+
+    return matches[1];
+}
+
+function getRootNamespaceFromFileName(csprojFileName: string) {
+    return path.basename(csprojFileName, path.extname(csprojFileName));
+}
+
+function getNamespace(rootNamespace: string, projectRootRelativePath: string) {
+    return path.join(rootNamespace, projectRootRelativePath)
+        .replace(/[\/\\]/g, '.')
+        .replace(/[^\w.]/g, '_')
+        .replace(/[.]{2,}/g, '.')
+        .replace(/^[.]+/, '')
+        .replace(/[.]+$/, '')
+        .split('.')
+        .map(s => /^[0-9]/.test(s) ? '_' + s : s)
+        .join('.');
+}
+
+function getCompletions(namespace: string) {
+    const moduleCompletion = new vscode.CompletionItem(namespace, vscode.CompletionItemKind.Module);
+
+    const snippetCompletion = new vscode.CompletionItem('namespace-fill', vscode.CompletionItemKind.Snippet);
+    snippetCompletion.insertText = namespace;
+    snippetCompletion.detail = namespace;
+
+    return [moduleCompletion, snippetCompletion];
 }
 
 export function deactivate() { }
